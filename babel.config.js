efab0e21efa6bd7b2a9af8cffd6f5e72ada296ac
truong.nguyen3 <truong.nguyen3@niteco.se>
2023-11-09 11:50:09 +0700

    add expo

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['babel-preset-expo'],
   plugins: [
     [
       'module-resolver',
